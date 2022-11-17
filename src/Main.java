33e8116ebf7b4db52a0ddd23e4ede1129dd9c5ee
Lenar Khafizov <lenarkh@mail.ru>
2022-11-17 22:58:09 +0300

    Первый вариант программы.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание. Коллекции HashSet и HashMap.");
+        String text = Text.text();
+        WordsChecker wordsChecker = new WordsChecker(text);
+        String word = "Incididunt";
+        if (wordsChecker.hasWord(word)) {
+            System.out.println("Текст содержит искомое слово: " + word);
+        } else {
+            System.out.println("Текст не содержит искомое слово: " + word);
+        }
     }
 }
---
+++ src/WordsChecker.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordsChecker {
+    protected String text;
+
+    public WordsChecker (String text) {
+        this.text = text;
+    }
+    public boolean hasWord (String word) {
+        String[] words = text.split("\\P{IsAlphabetic}+");
+        Set<String> wordsSet = new HashSet<>();
+        for (String wordFromText:words) {
+            wordsSet.add(wordFromText.toLowerCase());
+        }
+        return wordsSet.contains(word.toLowerCase());
+    }
+}
